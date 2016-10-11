a395bfe2420e7df98b0a2e5678644c2d6cf23b68
Emma Yeager <emmayeager0@gmail.com>
2016-10-11 11:53:18 +0200

    commit

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,5 +1,7 @@
 import { Mongo } from 'meteor/mongo';
  
-export const Timer = new Mongo.Collection('timer');
+//export const Timer = new Mongo.Collection('timer');
 
-export const Table = new Mongo.Collection('table');
+export const Table = new Mongo.Collection(null);
+
+
