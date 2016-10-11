fe51b66fb39cc5749a9080f4b43b05daf46810bf
Emma Yeager <emmayeager0@gmail.com>
2016-10-11 11:53:26 +0200

    commit

--- server/main.js
+++ server/main.js
@@ -1,28 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 
-import {Timer} from '../imports/api/tasks.js';
 import {Table} from '../imports/api/tasks.js';
 
 Meteor.startup(() => {
-    //counter value
-//    if (Timer.find().count() == 0) {
-//      Timer.insert( {number:1} ); //initialize count value 1
-//    }
-    //SyncedCron.start(); //call syncronization function
 
+  var headerData = ['AMC_HOT_SWAP','0x0','discrete','0x0100','na1','na2','na3','na4','na5','na6'];
 
-//SyncedCron.add({  //added with code 'meteor add percolate:synced-cron'
-//    name: 'Constantly update counter',
-//    schedule: function(parser) {  //parser is a later.parse object
-//      return parser.text('every 5 sec'); //run function every second
-//    },
-//    job: function() {
-//     var record = Timer.findOne();
-//      var number = record.number;
-//      number += 1;
+  var mainData = [
+    ['+12V',12.475,'Volts','ok',9.934,'7.932','5.930','13.861','15.863','18.866'],
+    ['+3.3V MMC', '33.850', 'Volts', 'ok', '29.845', '30.785', '31.960', '34.780', '35.955', '36.895'],
+    [],
+    [],
+    [],
+  ];
 
-//      return Timer.update({_id: record._id}, {$set: {number: number}});
-//   },
+  Table.insert({
+    header: headerData,
+    main: mainData
+  });
+
+  console.log(Table.findOne({}).header);
+  console.log(Table.findOne({}).main);
+
 });
 
 
